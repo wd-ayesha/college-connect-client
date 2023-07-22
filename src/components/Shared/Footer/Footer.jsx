3131b43453e677cf4de6802f25c2092c14e13eae
Ayesha <ayesha18mbstu@gmail.com>
2023-07-22 13:54:21 +0600

    footer added

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -2,9 +2,41 @@
 
 const Footer = () => {
     return (
-        <div>
-            
+        <footer>
+        <div className="footer p-10 bg-base-200 text-base-content">
+          <div>
+            <p className="text-3xl font-bold">
+            <span>College</span><span className="text-orange-900">Connect</span>
+            </p>
+            <p>Providing reliable service since 2023</p>
+          </div>
+          <div>
+            <span className="footer-title">Services</span>
+            <a className="link link-hover">Branding</a>
+            <a className="link link-hover">Design</a>
+            <a className="link link-hover">Marketing</a>
+            <a className="link link-hover">Advertisement</a>
+          </div>
+          <div>
+            <span className="footer-title">Company</span>
+            <a className="link link-hover">About us</a>
+            <a className="link link-hover">Contact</a>
+            <a className="link link-hover">Jobs</a>
+            <a className="link link-hover">Press kit</a>
+          </div>
+          <div>
+            <span className="footer-title">Legal</span>
+            <a className="link link-hover">Terms of use</a>
+            <a className="link link-hover">Privacy policy</a>
+            <a className="link link-hover">Cookie policy</a>
+          </div>
         </div>
+        <div className="footer footer-center p-4 bg-base-300 text-base-content">
+          <div>
+            <p>Copyright © 2023 - All right reserved by College Connect.</p>
+          </div>
+        </div>
+      </footer>
     );
 };
 
